b00f2657648247f29bad5ca1e873f77ad8da5b09
Ying <yingwitmon.win@gmail.com>
2024-10-01 13:13:05 +0700

    Add dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "@/styles/global.css";
 import { router } from "./libs/router";
 
 import { RouterProvider } from "react-router-dom";
