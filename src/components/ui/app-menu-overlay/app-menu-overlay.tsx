e15208b4ec2238afce75e437be760ab57de4f691
Ying <yingwitmon.win@gmail.com>
2025-06-26 17:21:41 +0700

    fix: add close button text to AppMenuOverlay and clean up GameMenu props

--- src/components/ui/app-menu-overlay/app-menu-overlay.tsx
+++ src/components/ui/app-menu-overlay/app-menu-overlay.tsx
@@ -5,29 +5,30 @@ import { ReactNode } from "react";
 
 interface AppMenuOverlayProps {
   children?: ReactNode;
+  closeBtnText?: string;
   isOpen?: boolean;
   onClose: () => void;
 }
 
 export const AppMenuOverlay = (props: AppMenuOverlayProps) => {
-  const { isOpen, onClose, children } = props;
+  const { isOpen, onClose, children, closeBtnText } = props;
 
   return (
     <nav
       className={cn(
-        "fixed h-screen z-50 inset-0 bg-slate-900 duration-300",
+        "fixed h-screen z-50 inset-0 bg-slate-950 duration-300",
         !isOpen && "opacity-0 pointer-events-none"
       )}
     >
       <div className="max-w-screen-2xl h-full mx-auto flex flex-col">
-        <div className="flex justify-start py-4 px-8">
+        <div className="py-4 px-8 h-18">
           <AppButton
-            variant="icon"
-            prefixIcon={
-              <XMarkIcon className="size-8 hover:text-yellow-500 transition-colors duration-300" />
-            }
+            variant="link"
+            prefixIcon={<XMarkIcon className="size-8" />}
             onClick={onClose}
-          />
+          >
+            {closeBtnText || ""}
+          </AppButton>
         </div>
         {children}
       </div>
